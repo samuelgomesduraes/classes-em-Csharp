5675a9d68fef11dcfb569ae442a4e824a236e452
samuelgomesduraes <sam710426@gmail.com>
2023-04-08 15:14:32 -0300

    up

---
+++ classes csharp/personagembase.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class personagembase : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+	public int vida=5;
+
+}
